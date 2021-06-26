ac3b410981db55631a91a1c3c756eae0ea90c2b9
Caleb Meador <meadorjc@gmail.com>
2021-06-26 16:30:38 +0000

    .files included, About edited

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,6 @@
   <div class="about">
     <h1>This is an about page</h1>
     <p>This page was created as a proof of concept using this <a href="https://scotch.io/tutorials/building-a-modern-app-using-nestjs-mongodb-and-vuejs">tutorial</a></p>
+    <p>The Maps page was implemented using <a href="https://www.positronx.io/how-to-implement-and-show-google-map-in-vue-js-app/">this</a> tutorial as a guide.</p>
   </div>
 </template>
